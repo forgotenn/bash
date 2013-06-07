e0822a719a8fb8699ab64c3bee374a42fa1adcd7
Malova Anna <an.forgotenn@gmail.com>
2013-06-08 02:56:21 +0400

    commit #2

--- poll/test.c
+++ poll/test.c
@@ -3,6 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 
 typedef struct pollfd pollfd;
 
@@ -19,14 +21,14 @@ int main()
         perror("open urandom");
     fds[1].fd = 0;
     fds[2].fd = 1;
-    
+
     fds[0].events  = POLLIN;
     fds[1].events = POLLIN;
     fds[2].events = POLLOUT;
-
     ret = poll(fds, 3, timeout_msecs);
     if (ret < 0)
         perror("poll");
+    printf("Poll\n");
     int buf_size[2];
     buf_size[0] = 0;
     buf_size[1] = 0;     
@@ -35,22 +37,25 @@ int main()
         int i; 
         for (i = 0; i < 3; i++)
         {
-            if ((fds[i].events & POLLIN) && (fds[i].revents & POLLOUT)) 
+            if (fds[i].revents & POLLOUT) 
             {
                 if (buf_size[i] < 100)
                 {    
+                    printf("%s %d\n", "size to read ", 100-buf_size[i]);
                     int tmp  = read(fds[i].fd, buf[i] + buf_size[i], 100 - buf_size[i]);
+                    printf("%s %d\n", "read", tmp);
                     if (tmp < 0)
                         perror("read");
                     buf_size[i] += tmp;
                 }
             }
-            if ((fds[i].events & POLLOUT) && (fds[i].revents & POLLOUT))
+            if (fds[i].revents & POLLOUT)
             {
                 int j;
                 for (j = 0; j < 2; j++) 
                 {
                     int tmp = write(fds[i].fd, buf[j], buf_size[j]);
+                    printf("%s %d\n", "write", tmp);
                     if (tmp < 0)
                         perror("write");
                     memmove(buf[j] + tmp, buf[j], buf_size[j] - tmp);
