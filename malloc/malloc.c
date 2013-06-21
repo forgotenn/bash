24165d8776bc716b01d21d120b3f017d0c9e9d59
Malova Anna <an.forgotenn@gmail.com>
2013-06-22 02:59:40 +0400

    big malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -5,9 +5,14 @@
 #include <stdint.h>
 
 const int PAGESIZE = 4096;
+size_t get_size(size_t size)
+{
+    return (size + sizeof(size_t) / PAGESIZE + 1) * PAGESIZE;
+}
+
 void* malloc(size_t size)
 {
-    void* mem = mmap(NULL, (size + sizeof(size_t) / PAGESIZE + 1) * PAGESIZE, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    void* mem = mmap(NULL, get_size(size), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     printf("malloc: %ld\n", size);
     printf("%lx\n", (intptr_t)mem);
     size_t* mem_size_t = (size_t*) mem;
@@ -19,6 +24,11 @@ void* malloc(size_t size)
 
 void free(void* ptr)
 {
+    if (ptr == NULL)
+        return;
+    size_t* mem_size_t = (size_t*) ptr;
+    if (!munmap(mem_size_t, get_size(*(mem_size_t - 1))))
+        perror("unmap");
 }
 
 void* calloc(size_t nmemb, size_t size)
